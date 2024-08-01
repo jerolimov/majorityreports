f4e506c411ab4460412d6fea7a46787a84651f1a
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-08-01 22:19:36 +0200

    ✨ fix #18: add experimental api to get latest items 🔮

--- api/src/reports/__init__.py
+++ api/src/reports/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .random import router as random
 from .most_events import router as most_events
 from .most_feedback import router as most_feedback
+from .latest_events import router as latest_events
 
 router = APIRouter()
 
 router.include_router(random, prefix="/random", tags=["random"])
-router.include_router(most_events, prefix="/most_events", tags=["most_feedback"])
+router.include_router(most_events, prefix="/most_events", tags=["most_events"])
 router.include_router(most_feedback, prefix="/most_feedback", tags=["most_feedback"])
+router.include_router(latest_events, prefix="/latest_events", tags=["latest_events"])
