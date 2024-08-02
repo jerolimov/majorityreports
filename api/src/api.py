805e59389829826466a9454d534745cd767cbc9b
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-08-02 22:56:15 +0200

    ✨ add simple stats api 🔮

--- api/src/api.py
+++ api/src/api.py
@@ -7,6 +7,7 @@ from .events import router as events
 from .feedbacks import router as feedbacks
 
 from .reports import router as reports
+from .stats import router as stats
 
 router = APIRouter()
 
@@ -17,6 +18,7 @@ router.include_router(actors, prefix="/actors", tags=["actors"])
 router.include_router(items, prefix="/items", tags=["items"])
 router.include_router(events, prefix="/events", tags=["events"])
 router.include_router(feedbacks, prefix="/feedbacks", tags=["feedbacks"])
+router.include_router(stats, prefix="/stats", tags=["stats"])
 
 router.include_router(
     actors, prefix="/namespace/{namespace_name}/actors", tags=["actors"]
@@ -28,6 +30,7 @@ router.include_router(
 router.include_router(
     feedbacks, prefix="/namespace/{namespace_name}/feedbacks", tags=["feedbacks"]
 )
+router.include_router(stats, prefix="/namespace/{namespace_name}/stats", tags=["stats"])
 
 # reports
 router.include_router(
