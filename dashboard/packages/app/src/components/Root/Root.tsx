ee9056d451baeaa57c15a095fd2e6d5fbde09038
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-08-03 22:59:16 +0200

    ✨ cleanup navigation 🔮

--- dashboard/packages/app/src/components/Root/Root.tsx
+++ dashboard/packages/app/src/components/Root/Root.tsx
@@ -1,58 +1,35 @@
 import React, { PropsWithChildren } from 'react';
-import { makeStyles } from '@material-ui/core';
 
-import HomeIcon from '@material-ui/icons/Home';
+import MenuIcon from '@material-ui/icons/Menu';
 import NamespaceIcon from '@material-ui/icons/AccountTree';
 import ActorIcon from '@material-ui/icons/Group';
 import ItemIcon from '@material-ui/icons/Description';
 import EventIcon from '@material-ui/icons/EventNote';
 import FeedbackIcon from '@material-ui/icons/RateReview';
 
-import LogoFull from './LogoFull';
-import LogoIcon from './LogoIcon';
-import {
-  Settings as SidebarSettings,
-  UserSettingsSignInAvatar,
-} from '@backstage/plugin-user-settings';
 import {
   Sidebar,
-  sidebarConfig,
   SidebarDivider,
   SidebarGroup,
   SidebarItem,
   SidebarPage,
   SidebarScrollWrapper,
-  SidebarSpace,
   useSidebarOpenState,
   Link,
 } from '@backstage/core-components';
-import MenuIcon from '@material-ui/icons/Menu';
 
-const useSidebarLogoStyles = makeStyles({
-  root: {
-    width: sidebarConfig.drawerWidthClosed,
-    height: 3 * sidebarConfig.logoHeight,
-    display: 'flex',
-    flexFlow: 'row nowrap',
-    alignItems: 'center',
-    marginBottom: -14,
-  },
-  link: {
-    width: sidebarConfig.drawerWidthClosed,
-    marginLeft: 24,
-  },
-});
+// import {
+//   Settings as SidebarSettings,
+//   UserSettingsSignInAvatar,
+// } from '@backstage/plugin-user-settings';
 
 const SidebarLogo = () => {
-  const classes = useSidebarLogoStyles();
   const { isOpen } = useSidebarOpenState();
 
   return (
-    <div className={classes.root}>
-      <Link to="/" underline="none" className={classes.link} aria-label="Home">
-        {isOpen ? <LogoFull /> : <LogoIcon />}
-      </Link>
-    </div>
+    <Link to="/" underline="none" aria-label="Home">
+      {isOpen ? <h1>✨ majority reports 🔮</h1> : <h3>🔮</h3>}
+    </Link>
   );
 };
 
@@ -62,8 +39,8 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
       <SidebarLogo />
       <SidebarDivider />
       <SidebarGroup label="Menu" icon={<MenuIcon />}>
-        <SidebarItem icon={HomeIcon} to="/" text="Home" />
-        <SidebarDivider />
+        {/* <SidebarItem icon={HomeIcon} to="/" text="Home" /> */}
+        {/* <SidebarDivider /> */}
         <SidebarScrollWrapper>
           <SidebarItem icon={NamespaceIcon} to="/namespaces" text="Namespaces" />
           <SidebarItem icon={ActorIcon} to="/actors" text="Actors" />
@@ -72,7 +49,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
           <SidebarItem icon={FeedbackIcon} to="/feedbacks" text="Feedback" />
         </SidebarScrollWrapper>
       </SidebarGroup>
-      <SidebarSpace />
+      {/* <SidebarSpace />
       <SidebarDivider />
       <SidebarGroup
         label="Settings"
@@ -80,7 +57,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         to="/settings"
       >
         <SidebarSettings />
-      </SidebarGroup>
+      </SidebarGroup> */}
     </Sidebar>
     {children}
   </SidebarPage>
